277cdeead063ec48f921af1a84c7d69820de35d4
anggraeniastri <astrianggraeni22@yahoo.co.id>
2017-03-14 23:46:31 +0700

    Add files via upload

---
+++ tokotas.java
@@ -0,0 +1,16 @@
+public class tokotas {
+    public static void main(String[] args) {
+        tokotas merk = new tokotas () ;
+        merk.merkTampilkan ();
+        System.out.println(merk.kembalikanNilai());
+        
+}
+private void merkTampilkan() {
+         System.out.println("Dior 1");
+         }
+
+private int kembalikanNilai () {
+         return 1;
+         }
+}
+
